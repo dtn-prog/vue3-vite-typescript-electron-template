95dcf896309e8ebc2fa9ee023808aa7059e0038e
dtn-prog <dtncode@gmail.com>
2025-07-28 20:30:41 +0700

    default recommended set up for main electron

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import { isDev } from './util.js'
 
-app.on('ready', ()=>{
+const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
   });
 
@@ -12,5 +12,20 @@ app.on('ready', ()=>{
     mainWindow.loadFile(path.join(app.getAppPath(), 'dist-vue/index.html'));
   }
 
+  return mainWindow;
+}
 
+app.on('ready', () => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin')
+    app.quit();
 })
